7ab02a5076edddb677c7c45d6c04001b2ada2750
iammeechar <aldomicha@gmail.com>
2021-04-29 15:29:09 +0300

    add jquery and js elements

---
+++ js/script.js
@@ -0,0 +1,68 @@
+function info( date, time, name, number ){
+  this.date = date;
+  this.time = time;
+  this.name = name;
+  this.number= number;
+};
+
+function customerDetails() {
+
+  event.preventDefault();
+  //Variable declaration
+  var newDate = document.getElementById("date").value;
+  var newTime = document.getElementById("time").value;
+  var newName = document.getElementById("name").value;
+  var newNumber= document.getElementById("number").value;
+  
+  var booked;
+  
+
+   if (document.getElementById("date").value === "d1") {
+      alert("Please your preffered date:");
+  }  
+   else if (document.getElementById("time").value === "t1") {
+      alert("Please choose your time of prefference:");
+  }  
+   else if (document.getElementById("name").value === "nm1") {
+      alert("Please fill in you name:"); 
+  } 
+   else if (newNumber === "") {
+      alert("Please fill in the space with your phone number:");
+      
+  } 
+   else {
+
+      var newInfo = new info(newDate, newTime, newName, newNumber);
+
+
+
+      $(document).ready(function() {
+          $("ol").append("<li>" + " Your booking for " + newInfo.date + " at " + newInfo.time + " for  " + newInfo.name + " and contact " + newInfo.number + "</li>");
+      });
+
+      alert("Your booking has been recorded and we will get back to you shortly");
+
+
+  }
+
+  if(newInfo.date >= "08:00:00 AM" && newInfo.date <= "5:00:00 PM")
+  {
+    alert("We are open!")
+  }
+  else if(newInfo.date <= "07:59:59" && newInfo.date >+"05:00:01 PM" ){
+    alert("We are closed!") 
+  }
+  
+  document.getElementById("booking").innerHTML =  present ;
+  document.getElementById("time").value = "d1";
+  document.getElementById("date").value = "t1";
+  document.getElementById("name").value = "na1";
+  document.getElementById("number").value = '';
+  event.preventDefault()
+}
+$(document).ready(function() {
+  $("#toggle").click(function() {
+      $("ol").slideUp(500).slideDown(500);
+      $("#booking").slideUp(500).slideDown(500);
+  });
+});
